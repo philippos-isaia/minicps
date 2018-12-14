9e9cd77c4241867b43f08245c3458c876178fa5f
philippos-isaia <philippos.isaia@gmail.com>
2018-12-14 10:42:29 +0200

    Installation File

---
+++ install.sh
@@ -0,0 +1,13 @@
+apt-get update
+apt-get install python make mininet openvswitch-testcontroller xterm python-pip
+sudo pip install --upgrade pip
+sudo -H pip install --upgrade pip
+sudo -H pip install pipenv
+sudo pip install --upgrade setuptools
+sudo pip install twisted
+
+sudo python setup.py build
+sudo python setup.py install
+
+sudo mkdir -p /usr/local/lib/python2.7/dist-packages/minicps/pymodbus/
+sudo cp minicps/minicps/pymodbus/* /usr/local/lib/python2.7/dist-packages/minicps/pymodbus/
